ec9dd7c7b9b98d8b903c1bcd39d0483ad16c8dc9
1Choq <58182184+1choqspark@users.noreply.github.com>
2020-11-20 20:54:46 -0600

    Add files via upload

---
+++ clasesFran2.0/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+ <?php
+    include("header.html");
+    ?>
+<body background="img/fondo2.jpg" style="background: url(img/fondo2.jpg) no-repeat center center fixed; background-size: cover;">
+	<div class="row col-12" style="position: absolute;top:100px;margin: auto;">
+		<!--caja de tamaño completo en anchura-->
+		<div class= "mr-auto col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+		<br>
+			<p align="center" style="color:white; font-size: xx-large;">lavandería lavamad</p>
+		</div><!--caja de tamaño medio en anchura izquierda-->
+		<div class= "ml-auto col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+			<p align="left" style="color: white; font-size: large;">¿Ya tienes cuenta? 
+				<a href="#">¡Ingresa aquí!</a></p>
+			<br>
+			<p align="left" style="color: white; font-size: large;">Quieres consultar tu información con respecto al servicio de lavandería <a href="#">¡Crea una cuenta para poder iniciar sesión!</a></p>
+		</div><!--caja de tamaño medio en anchura derecha-->
+		<div class= "mr-auto col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+			<p align="left" style="color: white; font-size: large;">¿Tienes alguna duda o requieres alguna información? <a href="#">¡Visita Nuestra sección de Información!</a></p>
+		</div><!--caja de tamaño completo en anchura-->
+	</div>
+</body>
+<?php
+include ("footer.html");
+?>
+</html>
